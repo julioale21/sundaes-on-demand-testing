be076f90408c538300d0a6eb644b816129b17899
julioale21 <30907777+julioale21@users.noreply.github.com>
2021-11-08 12:47:42 -0300

    added provider to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
-import SummaryForm from './pages/summary/SummaryForm';
-
+import Container from 'react-bootstrap/Container';
+import OrderEntry from './pages/entry/OrderEntry';
+import { OrderDetailsProvider} from './contexts/OrderDetails';
 function App() {
   return (
-    <div>
-      <SummaryForm />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        {/* Summary page and entry page need provider */}
+        <OrderEntry />
+      </OrderDetailsProvider>
+      {/* confirmation page does not need provider */}
+    </Container>
   );
 }
 
